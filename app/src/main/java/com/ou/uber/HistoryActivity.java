a9ec77bace5da0d3ff486051631dac4a158db73f
quangtran9kz <quangtran10kz@gmail.com>
2019-04-06 13:59:45 +0700

    First

---
+++ app/src/main/java/com/ou/uber/HistoryActivity.java
@@ -0,0 +1,84 @@
+package com.ou.uber;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.ou.uber.historyRecyclerView.HistoryAdapter;
+import com.ou.uber.historyRecyclerView.HistoryObject;
+
+import java.util.ArrayList;
+
+public class HistoryActivity extends AppCompatActivity {
+    private String customerOrDriver, userId;
+
+    private RecyclerView mHistoryRecyclerView;
+    private RecyclerView.Adapter mHistoryAdapter;
+    private RecyclerView.LayoutManager mHistoryLayoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+
+        mHistoryRecyclerView = (RecyclerView) findViewById(R.id.historyRecyclerView);
+        mHistoryRecyclerView.setNestedScrollingEnabled(false);
+        mHistoryRecyclerView.setHasFixedSize(true);
+        mHistoryLayoutManager = new LinearLayoutManager(HistoryActivity.this);
+        mHistoryRecyclerView.setLayoutManager(mHistoryLayoutManager);
+        mHistoryAdapter = new HistoryAdapter(getDataSetHistory(), HistoryActivity.this);
+        mHistoryRecyclerView.setAdapter(mHistoryAdapter);
+
+
+        customerOrDriver = getIntent().getExtras().getString("customerOrDriver");
+        userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        getUserHistoryIds();
+    }
+
+    private void getUserHistoryIds() {
+        DatabaseReference userHistoryDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(customerOrDriver).child(userId).child("history");
+        userHistoryDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()){
+                    for(DataSnapshot history : dataSnapshot.getChildren()){
+                        FetchRideInformation(history.getKey());
+                    }
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+    }
+
+    private void FetchRideInformation(String rideKey) {
+        DatabaseReference historyDatabase = FirebaseDatabase.getInstance().getReference().child("history").child(rideKey);
+        historyDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()){
+                    String rideId = dataSnapshot.getKey();
+                    HistoryObject obj = new HistoryObject(rideId);
+                    resultsHistory.add(obj);
+                    mHistoryAdapter.notifyDataSetChanged();
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+    }
+
+    private ArrayList resultsHistory = new ArrayList<HistoryObject>();
+    private ArrayList<HistoryObject> getDataSetHistory() {
+        return resultsHistory;
+    }
+}
